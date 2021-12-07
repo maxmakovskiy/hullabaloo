fbe246c38e7d7f5e459f57d2bc4bcf4c407ae5ba
maxmakovskiy <makovskiymax@gmail.com>
2021-12-08 00:17:38 +0200

    Updated with more complex example

--- basic_types/using_import.hs
+++ basic_types/using_import.hs
@@ -2,7 +2,9 @@ module UsingImport where
 
 import Data.Char
 
-thisCharIsDigit = isDigit '3'
-
+-- works only for 10 =< x < 100 numbers
+-- if both characters is digits then return number else return 100
+twoDigits2Int :: Char -> Char -> Int
+twoDigits2Int x y = if ((isDigit x) && (isDigit y)) then digitToInt x * 10 + digitToInt y else 100
 
 
