567e36d92ca3d56e44b774ad5a00331167992ade
maxmakovskiy <makovskiymax@gmail.com>
2021-12-07 22:49:44 +0200

    Create sum of squares opertor

---
+++ operators/define_own_operator.hs
@@ -0,0 +1,13 @@
+module OwnSumSquresOperator where
+
+-- declaration
+-- syntax infix(l/r) [priority] [operator_declaration] defines priority 
+-- and associative of side [l/r]
+infixl 6 *+*
+
+-- definition
+a *+* b = a ^ 2 + b ^ 2
+-- another form - function like
+-- (*+*) a b = a ^ 2 + b ^ 2
+
+
