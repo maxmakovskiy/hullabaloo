ea697f159a9410f8883f85ed83d89c64b429ea5f
maxmakovskiy <makovskiymax@gmail.com>
2021-12-16 19:16:23 +0200

    Error and Undified

--- recursion/factorial.hs
+++ recursion/factorial.hs
@@ -7,4 +7,6 @@ factorial n = if n == 0 then 1 else n * factorial (n - 1)
 factorial' 0 = 1
 factorial' n = n * factorial' (n - 1)
 
-
+-- error info 
+factorial'' 0 = 1
+factorial'' n = if n < 0 then error "argument must be greather than 0" else n * factorial'' (n - 1)
