39db8d9a97f461fb8eb71af15660b1466796f191
maxmakovskiy <makovskiymax@gmail.com>
2021-12-16 22:05:55 +0200

    Optimazed

--- recursion/fibonacci.hs
+++ recursion/fibonacci.hs
@@ -5,3 +5,11 @@ fibonacci n | n == 0 = 0
             | n > 0 = fibonacci (n - 1) + fibonacci (n - 2)
             | n < 0 = fibonacci (n + 2) - fibonacci (n + 1)
 
+fibAux n res prev
+    | n == 0 = res
+    | otherwise = fibAux (n-1) (res+prev) res
+
+fibonacciOpt n
+    | n == 0 = 0
+    | otherwise = fibAux n 1 0
+
