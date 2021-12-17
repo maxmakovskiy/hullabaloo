37f34a5d522a6ba577b6f56e1acfc27401a4cdd6
maxmakovskiy <makovskiymax@gmail.com>
2021-12-18 00:08:08 +0200

    Using lamda like high order

--- high_order_func/if_even.hs
+++ high_order_func/if_even.hs
@@ -9,10 +9,14 @@ ifEven func x = if even x
 inc x = x + 1
 double x = x * 2
 square x = x ^ 2
-cube x = x ^ 3
+-- cube x = x ^ 3
 
 ifEvenInc x = ifEven inc x 
 ifEvenDouble x = ifEven double x 
 ifEvenSquare x = ifEven square x 
-ifEvenCube x = ifEven cube x
+-- ifEvenCube x = ifEven cube x
+ifEvenCube x = ifEven (\x -> x ^ 3) x
 ifEvenNegate x = ifEven negate x -- negate from Prelude
+
+-- Using lamda as operation's function
+ifEvenDec x = ifEven (\x -> x - 1) x
