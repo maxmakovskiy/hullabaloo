52cd3cc5544caa47931dec8f98b4570f2ad979b5
maxmakovskiy <makovskiymax@gmail.com>
2021-12-18 00:04:52 +0200

    add more operations

--- high_order_func/if_even.hs
+++ high_order_func/if_even.hs
@@ -1,6 +1,6 @@
 module IfEven where
 
--- General computation template
+-- This example may named as 'General computation template'
 
 -- Prelude.even
 ifEven func x = if even x
@@ -9,9 +9,10 @@ ifEven func x = if even x
 inc x = x + 1
 double x = x * 2
 square x = x ^ 2
+cube x = x ^ 3
 
 ifEvenInc x = ifEven inc x 
 ifEvenDouble x = ifEven double x 
 ifEvenSquare x = ifEven square x 
-
-
+ifEvenCube x = ifEven cube x
+ifEvenNegate x = ifEven negate x -- negate from Prelude
