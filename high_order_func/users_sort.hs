6f9c19bd1347782b7c466f9d6e41e77f548b1394
maxmakovskiy <makovskiymax@gmail.com>
2021-12-18 00:43:38 +0200

    Give func like arg to standart function List.sortBy

---
+++ high_order_func/users_sort.hs
@@ -0,0 +1,33 @@
+module UsersSort where
+
+import Data.List
+
+-- Sample data
+names = [("Adams", "Baker"),
+        ("Irwin", "Jones"),
+        ("Ghosh", "Hills"),
+        ("Mason", "Nalty"),
+        ("Evans", "Frank"),
+        ("Ochoa", "Patel"),
+        ("Klein", "Lopez"),
+        ("Clark", "Davis"),
+        ("Quinn", "Reily")]
+
+-- Create function that will be taked to List.sortBy
+compareBySecondName pair1 pair2 = if lname1 > lname2
+    then GT
+    else
+        if lname1 < lname2
+        then LT
+        else 
+            if fname1 > fname1
+            then GT
+            else
+                if fname1 < fname2
+                then LT
+                else EQ
+    where lname1 = snd pair1
+          lname2 = snd pair2
+          fname1 = fst pair1
+          fname2 = fst pair1
+    
