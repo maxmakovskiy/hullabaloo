84e9170f970d242f8418b2d77713fc23b8535680
maxmakovskiy <makovskiymax@gmail.com>
2021-12-25 00:02:28 +0200

    Pattern matching : basics on the lists

---
+++ pattern_matching_for_recursion/basics.hs
@@ -0,0 +1,19 @@
+module PMBasics where
+-- Pattern Matching
+
+-- Is list empty?
+isEmpty [] = True
+isEmpty _ = False
+
+-- Return head of given list
+-- Name agreement in Haskell: x - head of list, xs - all list
+myHead (x:xs) = x
+-- situation of empty list
+myHead [] = error "Given list doesn't have a head"
+
+-- Return tail of list
+-- Use _ for value that doesn't interesting us
+myTail (_:xs) = xs
+
+
+
